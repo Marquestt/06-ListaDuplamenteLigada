e4bd9f20363a058e254dc49e7bed74cfb586f225
Marquestt <thatyanecmarques98@live.com>
2023-04-04 11:12:08 -0300

    Implementadas as funções excluirUltimoElemento, excluirPrimeiroElemento e exibirReverso

--- ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
+++ ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
@@ -150,17 +150,48 @@ void inserirElemento()
 // funções a serem implementadas no exericio
 void exibirReverso()
 {
-
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = ultimo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->ant;
+		}
+	}
 }
 
 void excluirPrimeiroElemento()
 {
-
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		NO* aux = primeiro;
+		primeiro = primeiro->prox;
+		primeiro->ant = NULL;
+		free(aux);
+		cout << "Elemento excluido\n";
+	}
 }
 
 void excluirUltimoElemento()
 {
-
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		NO* aux = ultimo;
+		ultimo = ultimo->ant;
+		ultimo->prox = NULL;
+		free(aux);
+		cout << "Elemento excluido\n";
+	}
 }
 
 
